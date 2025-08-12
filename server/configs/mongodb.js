2ab12d93e666158495f47bddcb2e76980c4689fc
yosii <yosefasefa8889@gmail.com>
2025-08-12 12:44:51 +0300

    yulka

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -1,9 +1,7 @@
 import mongoose from "mongoose";
 // Connect to the MongoDB database
-const connectDB =async()=>{
-    mongoose.connection.on('connected', ()=> console.log('Database Connected'))
+const connectDB = async () => {
+    mongoose.connection.on('connected', () => console.log('Database Connected'))
     await mongoose.connect(`${process.env.MONGODB_URI}/LMI`)
-
 }
 export default connectDB
-
--- server/server.js
+++ server/server.js
@@ -6,20 +6,25 @@ import { clerkWebhooks } from './controllers/webhooks.js'
 import bodyParser from 'body-parser' // <-- import here
 
 const app = express()
-await connectDB()
 
-app.use(cors())
+// Wrap in an async IIFE to use await at the top level
+;(async () => {
+  await connectDB()
 
-app.get('/', (req, res) => res.send("API Working"))
+  app.use(cors())
 
-// Use raw body parser ONLY for this webhook route
-app.post(
-  '/clerk',
-  bodyParser.raw({ type: 'application/json' }),
-  clerkWebhooks
-)
+  app.get('/', (req, res) => res.send("API Working"))
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`)
-})
+  // Use raw body parser ONLY for this webhook route
+  app.post(
+    '/clerk',
+    bodyParser.raw({ type: 'application/json' }),
+    clerkWebhooks
+  )
+
+  const PORT = process.env.PORT || 5000
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
+  })
+})()
+
