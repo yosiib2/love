14e2542b043efc208b0be2ff6d4a61888eb97a49
yosii <yosefasefa8889@gmail.com>
2025-08-12 20:47:11 +0300

    hnnn

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ const app = express()
   
   app.use('/api/educator', express.json(), educatorRouter)  // <-- fixed here
 
-  const PORT = process.env.PORT || 5000
+  const PORT = process.env.PORT || 3000
   app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
   })
